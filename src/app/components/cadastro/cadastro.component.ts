f8e85f27ea26652cd0b5d3c1aa8aae9b5fde91d7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-14 11:28:00 -0300

    Adicionando rota para cadastro e relacionando campos dos inputs de cadastro

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  nome = '';
+  email = '';
+  senha = '';
+  conf = '';
+
   constructor() { }
 
   ngOnInit(): void {
