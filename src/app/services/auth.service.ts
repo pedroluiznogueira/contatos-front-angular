c242f4c0f7cd762ce0f201a87fda68b6a10cfb21
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-18 11:00:07 -0300

    função next (tem os dados), error (se tiver algum erro), complete (caso não tenha erro aqui faz algo, mas não contêm os dados do observable)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,26 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClientModule) { }
+  data?: Object;
+
+  constructor(private http: HttpClient) { }
 
   cadastrar(nome: string, email: string, senha: string) {
-    console.log(nome, email, senha)
+    let u = {nome, email, senha}
+    this.http.post("http://contatos-nodb.herokuapp.com/registrar", u)
+                  .subscribe(
+                    {
+                      next: data => console.log(data),
+                      error: err => console.log(err),
+                      complete: () => console.log("O observavel já não tem mais o que ser observado")
+                    }
+                  );
   }
 
 }
