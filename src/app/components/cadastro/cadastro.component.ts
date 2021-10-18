909369046b0cf7c31d7ccf8e428c3ea7bd3b95fd
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-18 11:37:38 -0300

    direcionando o usuario para a home após um cadastro

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -31,10 +31,12 @@ export class CadastroComponent implements OnInit {
     let observable = this.servico.cadastrar(this.nome, this.email, this.senha);
     observable.subscribe(
         {
-          next: data =>
-          // salvar o token no session storage
-          window.sessionStorage.setItem("token", (<response>data).token),
-          // direcionar o usuário para a home
+          next: data => {
+            // salvar o token no session storage
+            window.sessionStorage.setItem("token", (<response>data).token),
+            // direcionar o usuário para a home
+            this.router.navigateByUrl("/home")
+          },
 
           error: err => console.log(err),
           complete: () => console.log("O observavel já não tem mais o que ser observado")
